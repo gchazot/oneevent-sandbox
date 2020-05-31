d1fabf96767b98ef84d4df15b3965b8793114345
Germain Chazot <germain@edited.com>
2020-05-31 16:20:58 +0100

    Compute Facebook avatar URL

--- oneevent_site/context_processors.py
+++ oneevent_site/context_processors.py
@@ -4,16 +4,25 @@ from social_django.models import UserSocialAuth
 def social_auth_extra_data(request):
     social = _get_prefered_social(request.user)
 
-    if social is not None:
-        return {
-            'user_avatar_url': social.extra_data.get('user_avatar'),
-        }
-    return {}
+    if social is None:
+        return {}
+
+    return {
+        'user_avatar_url': _get_social_avatar_url(social),
+    }
+
+
+def _get_social_avatar_url(social):
+    if social.provider == 'facebook':
+        facebook_id = social.extra_data.get('id')
+        return f'http://graph.facebook.com/{facebook_id}/picture?type=small'
+    else:
+        return social.extra_data.get('user_avatar')
 
 
 def _get_prefered_social(user):
     if user and user.is_authenticated:
-        for provider in ('google-oauth2', 'github'):
+        for provider in ('google-oauth2', 'github', 'facebook'):
             try:
                 return user.social_auth.get(provider=provider)
             except UserSocialAuth.DoesNotExist:
